f8d097ac98c7c04b78831c5f9afde3b36f8c9913
Abdourahamane <abdourahamane.issakasani@usmba.ac.ma>
2021-05-25 17:31:40 +0100

    add welcome page & auth modals

--- app/base/base.py
+++ app/base/base.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from app.auth.forms import LoginForm
+
 base_bp = Blueprint('base_bp', __name__, template_folder='templates', static_folder='static')
 
 
 @base_bp.route('/', methods=['GET', 'POST'])
 def base():
-    return render_template('welcome.html', title='Welcome')
+    login_form = LoginForm()
+    return render_template('welcome.html', title='Welcome', form=login_form)
